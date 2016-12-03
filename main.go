601b5ce908647e5c080a2658b67fbe6153cc2c24
Kevin-Xi <kevin.xgr@gmail.com>
2016-12-03 15:27:05 +0800

    Add first server and notes.

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
+// under $GOPATH/{src}/{host}/{authorname}/{projectname}
+// so it can work with `go get ...`
 package main
 
+// dependency
+import "net/http"
+
 func main() {
-    println("hello!")
+    // HandlerFunc register "/" with hello for ServeMux(http router)
+    // "/" matches all path, not only the "/"
+    // first-class function
+    // [!] So can see this function change and save some internal states we cannot see here
+    http.HandleFunc("/", hello)     // (pattern, handler that been registered)
+    http.ListenAndServe(":8080", nil)
+}
+
+// with a type system of strong, static, inferred
+// http.HandlerFunc
+// server will spawn a new goroutine executing this for each request
+func hello(w http.ResponseWriter, r *http.Request) {
+    // w.Write take general []byte, so need to cast
+    // if lost ')', compiler will raise: ./main.go:19: syntax error: unexpected semicolon or newline, expecting comma or )
+    // it is ok
+    w.Write([]byte("hello!"))
 }
